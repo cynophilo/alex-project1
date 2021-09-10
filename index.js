91d8a7bc0a17672bc4265d803d51641758211987
Alexandre Parent <alexandreparent_dev@outlook.com>
2021-09-10 12:55:55 -0400

    Setup initial code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+let rest = 10 - 6.75;
+let value;
 
+console.log(Math.floor(rest / 10), 'x 10$');
+rest = rest % 10;
+console.log(Math.floor(rest / 5), 'x 5$');
+rest = rest % 5;
+console.log(Math.floor(rest / 2), 'x 2$');
+rest = rest % 2;
+console.log(Math.floor(rest / 1), 'x 1$');
+rest = rest % 1;
+console.log(Math.floor(rest / 0.25), 'x .25$');
+rest = rest % 0.25;
+console.log(Math.floor(rest / 0.10), 'x .10$');
+rest = rest % 0.10;
+console.log(Math.floor(rest / 0.05), 'x .05$');
+rest = rest % 0.05;
+console.log(Math.floor(rest / 0.01), 'x .01$');
+rest = rest % 0.01;
