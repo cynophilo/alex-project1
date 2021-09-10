afda9d90d88a206f2a9ee674dd5e80ae28903199
Alexandre Parent <alexandreparent_dev@outlook.com>
2021-09-10 14:29:43 -0400

    Fixup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let rest = 10 - 6.78;
+let rest = 10 - 6.61;
+
 console.log(Math.floor(rest / 10), 'x 10$');
 rest = rest % 10;
 console.log(Math.floor(rest / 5), 'x 5$');
@@ -11,12 +12,11 @@ console.log(Math.floor(rest / 0.25), 'x .25$');
 rest = rest % 0.25;
 console.log(Math.floor(rest / 0.10), 'x .10$');
 rest = rest % 0.10;
-console.log(Math.floor(rest >= 0.03 ? 1 : 0), 'x .05$')
-rest = rest % 0,05
+console.log(Math.floor(rest >= 0.03 ? 1 : 0), 'x .05$');
+rest = rest % 0.05;
 // nbr de piece à retourner: Math.floor(rest / 0.05);
 // reste apres: rest % 0.05;
 // Plus grand que 0 et plus petit 0.05 => ]0, 0.05[
 // {0.01, 0.02, 0.03, 0.04}
 // 0      0     1     1
 // (rest >= 0.03 ? 1 : 0)
-
