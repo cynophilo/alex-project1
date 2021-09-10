36b71f91c492ad59b6c86f42e116f5e92a5f5fec
Alexandre Parent <alexandreparent_dev@outlook.com>
2021-09-10 13:31:07 -0400

    Deduction

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let rest = 10 - 6.75;
-let value;
+
+// Arrondi au 0.05 la plus proche
+// Arrondi rest vers le haut si rest >= 0.03
+// Arrondi vers le bas si < 0.03
+//
 
 console.log(Math.floor(rest / 10), 'x 10$');
 rest = rest % 10;
@@ -13,7 +17,10 @@ console.log(Math.floor(rest / 0.25), 'x .25$');
 rest = rest % 0.25;
 console.log(Math.floor(rest / 0.10), 'x .10$');
 rest = rest % 0.10;
-console.log(Math.floor(rest / 0.05), 'x .05$');
-rest = rest % 0.05;
-console.log(Math.floor(rest / 0.01), 'x .01$');
-rest = rest % 0.01;
+// nbr de piece à retourner: Math.floor(rest / 0.05);
+// reste apres: rest % 0.05;
+// Plus grand que 0 et plus petit 0.05 => ]0, 0.05[
+// {0.01, 0.02, 0.03, 0.04}
+// 0      0     1     1
+// (rest >= 0.03 ? 1 : 0)
+console.log(, 'x .05$');
